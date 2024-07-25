eeb3b97070bed78673749b0aa1e6e72507321b86
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-07-25 11:51:11 +0530

    background ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -32,6 +33,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.palette:palette:1.0.0")
     implementation("com.karumi:dexter:6.2.2")
     implementation(libs.appcompat)
